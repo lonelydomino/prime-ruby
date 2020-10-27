0aec2d2a8449bc7a2de68b48d02614f382a1faf2
Michael Martinez <zeroeclipse@live.com>
2020-10-26 18:00:43 -0700

    commit tree 74596e0834d9c7b7516e7a1481ff047615d05312

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(int)
+    if int <= 1 
+        return false
+    end
+
+    for n in 2..(int - 1)
+        if (int % n) == 0
+           return false
+        end
+    end
+    
+    return true
+end
